4033429c3a6136505e2be7b64d76154081f46155
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-08-24 22:59:55 -0700

    update htmlcoin

--- App/Controllers/socket.js
+++ App/Controllers/socket.js
@@ -10,7 +10,7 @@ let InsightApiRepository = require("../Repositories/InsightApiRepository"),
 const Address = require('../Components/Address');
 const _ = require('lodash');
 const TokenBalanceChangeEvents = require('../Components/SocketEvents/TokenBalanceChangeEvents');
-const QtumRoomEvents = require('../Components/SocketEvents/QtumRoomEvents');
+const HtmlcoinRoomEvents = require('../Components/SocketEvents/HtmlcoinRoomEvents');
 const TokenContract = require('../Components/TokenContract');
 const ContractPurchaseEvents = require('../Components/SocketEvents/ContractPurchaseEvents');
 const contractPurchaseWatcherInstance = require("../Components/ContractPurchaseWatcherInstance");
@@ -51,7 +51,7 @@ class SocketController {
 
     initSocketEvents() {
         this.events.tokenBalanceEvents = new TokenBalanceChangeEvents(this.socket, this.tokenContract);
-        this.events.qtumRoomEvents = new QtumRoomEvents(this.socket, this.socketClient);
+        this.events.htmlcoinRoomEvents = new HtmlcoinRoomEvents(this.socket, this.socketClient);
         this.events.contractPurchaseEvents = new ContractPurchaseEvents(contractPurchaseWatcherInstance);
     }
 
@@ -116,7 +116,7 @@ class SocketController {
 
         socket.on('disconnect', () => {
 
-            this.events.qtumRoomEvents.unsubscribeAddress(socket, null);
+            this.events.htmlcoinRoomEvents.unsubscribeAddress(socket, null);
             this.events.tokenBalanceEvents.unsubscribeAddress(socket, null);
 
             logger.info('User disconnected', remoteAddress);
@@ -161,7 +161,7 @@ class SocketController {
             return false;
         }
 
-        this.events.qtumRoomEvents.subscribeAddress(socket, addresses);
+        this.events.htmlcoinRoomEvents.subscribeAddress(socket, addresses);
 
         if (options.notificationToken) {
             this.mobileAddressBalanceNotifier.subscribeAddress(addresses, options);
@@ -236,7 +236,7 @@ class SocketController {
             return false;
         }
 
-        this.events.qtumRoomEvents.unsubscribeAddress(socket, addresses);
+        this.events.htmlcoinRoomEvents.unsubscribeAddress(socket, addresses);
 
         if (options.notificationToken) {
             this.mobileAddressBalanceNotifier.unsubscribeAddress(options.notificationToken, addresses);
