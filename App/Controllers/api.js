6d03de02c79e047f9a37c1677ce46ce2b7d34d98
Jaron Rosenau <jaron.rosenau@gmail.com>
2018-08-24 22:36:32 -0700

    update htmlcoin

--- App/Controllers/api.js
+++ App/Controllers/api.js
@@ -37,7 +37,7 @@ let APIController = {
 		APIController.app.options('*', cors());
 
 
-        APIController.addHandler('get', '/qrc20/:contractAddress/transfers', Controllers.qrc20.fetchTransfers.bind(Controllers.qrc20));
+        APIController.addHandler('get', '/hrc20/:contractAddress/transfers', Controllers.hrc20.fetchTransfers.bind(Controllers.hrc20));
 
         APIController.addHandler('get', '/contracts/:contractAddress/exists', Controllers.contracts.exists.bind(Controllers.contracts));
 
